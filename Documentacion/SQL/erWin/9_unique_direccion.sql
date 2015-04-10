a873c77f9bd7bcc2ba534ccd6391eb6317a56198
Nayely <nvo.32002@gmail.com>
2015-04-10 11:22:33 -0500

    unique constraints dirección

---
+++ Documentacion/SQL/erWin/9_unique_direccion.sql
@@ -0,0 +1,15 @@
+CREATE  SEQUENCE seq_colonia INCREMENT  BY 1
+     MINVALUE 1  
+     START  WITH 1;
+
+CREATE  SEQUENCE seq_calle INCREMENT  BY 1
+     MINVALUE 1  
+     START  WITH 1;
+
+CREATE  SEQUENCE seq_codigopostal INCREMENT  BY 1
+     MINVALUE 1  
+     START  WITH 1;
+
+CREATE  SEQUENCE seq_direccion INCREMENT  BY 1
+     MINVALUE 1  
+     START  WITH 1;
